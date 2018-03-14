cb1208124bfcbcf1b7619ec57bc3d9e8ceae55df
Eric Potter <pottereric@hotmail.com>
2018-03-13 23:45:27 -0400

    Get the titles from the StirTrek page.

--- StirTrekKeywordAnalyzer/Program.fs
+++ StirTrekKeywordAnalyzer/Program.fs
@@ -1,7 +1,15 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open canopy
+
+let analyzeStirTrekTags _ =
+    start Chrome
+    pin FullScreen
+    url "https://www.stirtrek.com/Schedule?flat=False&year=2018"
+    let titles = elements ".sessionTitle"
+    titles |> List.iter (fun t -> printfn "%s" (read t))
+    ()
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    analyzeStirTrekTags()
+    System.Console.ReadKey() |> ignore
     0 // return an integer exit code
