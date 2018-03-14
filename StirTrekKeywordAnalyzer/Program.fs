64226835c1df1cc281fc73052453a45e441bde60
Eric Potter <pottereric@hotmail.com>
2018-03-13 23:56:53 -0400

    Run the titles through the Cognitive Services Key Phrase analyzer.

--- StirTrekKeywordAnalyzer/Program.fs
+++ StirTrekKeywordAnalyzer/Program.fs
@@ -1,11 +1,24 @@
 ﻿open canopy
+open Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models
+
+let azureRegion = AzureRegions.Westcentralus
+let subscriptionKey =  "<Your-Subscription-Key>"
+
 
 let analyzeStirTrekTags _ =
     start Chrome
-    pin FullScreen
+    //pin FullScreen
     url "https://www.stirtrek.com/Schedule?flat=False&year=2018"
     let titles = elements ".sessionTitle"
-    titles |> List.iter (fun t -> printfn "%s" (read t))
+    //titles |> List.iter (fun t -> printfn "%s" (read t))
+
+    let inputs = titles |> List.mapi(fun i t -> ("en", i.ToString(), (read t)))
+
+    let client = FognitiveServices.Text.Client.create subscriptionKey azureRegion
+    let results = FognitiveServices.Text.Client.getTags client inputs
+    
+    results.Documents |> Seq.iter (fun r -> r.KeyPhrases |> Seq.iter (fun kp -> printfn "%s %s" r.Id kp))
+
     ()
 
 [<EntryPoint>]
