b7083e1fa43a9f73e48ffecacbc08967f50ffe28
Eric Potter <pottereric@hotmail.com>
2018-03-14 09:28:27 -0400

    Code Cleanup

--- StirTrekKeywordAnalyzer/Program.fs
+++ StirTrekKeywordAnalyzer/Program.fs
@@ -7,19 +7,16 @@ let subscriptionKey =  "<Your-Subscription-Key>"
 
 let analyzeStirTrekTags _ =
     start Chrome
-    //pin FullScreen
     url "https://www.stirtrek.com/Schedule?flat=False&year=2018"
-    let titles = elements ".sessionTitle"
-    //titles |> List.iter (fun t -> printfn "%s" (read t))
-
-    let inputs = titles |> List.mapi(fun i t -> ("en", i.ToString(), (read t)))
 
     let client = FognitiveServices.Text.Client.create subscriptionKey azureRegion
-    let results = FognitiveServices.Text.Client.getTags client inputs
-    
-    results.Documents |> Seq.iter (fun r -> r.KeyPhrases |> Seq.iter (fun kp -> printfn "%s %s" r.Id kp))
 
-    ()
+    let results = 
+        elements ".sessionTitle" 
+        |> List.mapi(fun i t -> ("en", i.ToString(), (read t)))
+        |> FognitiveServices.Text.Client.getTags client 
+
+    results.Documents |> Seq.iter (fun r -> r.KeyPhrases |> Seq.iter (fun kp -> printfn "%s %s" r.Id kp))
 
 [<EntryPoint>]
 let main argv = 
